41f2d1aa44889d8f922638f2c926461dea2665dd
Zachary-Fujitsu <zsgoempire@gmail.com>
2023-02-18 23:50:39 +0800

    first commit

---
+++ ProgramStructure/AccessModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramStructure
+{
+    internal class InternalBaseClass
+    {
+        public static int intM = 0;
+    }
+
+    public class AccessModifier
+    {
+        InternalBaseClass myBase = new InternalBaseClass();
+
+    }
+}
---
+++ ProgramStructure/Program.cs
@@ -0,0 +1,16 @@
+﻿using ProgramStructure;
+using ProgramStructure2;
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var myBase = new BaseClass();   // CS0122  
+        Console.WriteLine(myBase.intM);
+    }
+}
+
+
